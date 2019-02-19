56fc552dd98a8d911495a8e7426595aa7f10860c
FJiang6 <jiangfan6@outlook.com>
2019-02-19 18:47:58 +0800

    react demo02

--- src/views/option02/option02.js
+++ src/views/option02/option02.js
@@ -1,8 +1,10 @@
 import React, {Component} from "react";
 import {Link} from "react-router-dom";
 import "./option02.scss";
-import Child1OfOption2 from "./children/child1OfOption2"
-import ContextExample from "./children/context/contextExample"
+import Child1OfOption2 from "./children/child1OfOption2";
+import ContextExample from "./children/context/contextExample";
+import ErrorBoundary from "./children/errorBoundaries/errorBoundary";
+import BuggyCounter from "./children/errorBoundaries/buggyCounter";
 
 
 class EssayForm extends React.Component {
@@ -90,6 +92,17 @@ class Option02 extends Component {
                         <input type='text' autoFocus/>
                         <Child1OfOption2></Child1OfOption2>
                         <ContextExample></ContextExample>
+                        <ErrorBoundary>
+                            <p>These two counters are inside the same error boundary. If one crashes, the error boundary
+                                will replace both of them.</p>
+                            <BuggyCounter/>
+                            <BuggyCounter/>
+                        </ErrorBoundary>
+                        <hr/>
+                        <p>These two counters are each inside of their own error boundary. So if one crashes, the other
+                            is not affected.</p>
+                        <ErrorBoundary><BuggyCounter/></ErrorBoundary>
+                        <ErrorBoundary><BuggyCounter/></ErrorBoundary>
                     </div>
                     <div className='right'>
                         <p>
