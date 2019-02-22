2898d01113e49079646c3e216974428c6fe3d2cd
FJiang6 <jiangfan6@outlook.com>
2019-02-22 17:30:58 +0800

    Optimizing Performance

--- src/views/option03/option03.js
+++ src/views/option03/option03.js
@@ -4,17 +4,23 @@ import fragments from './children/columns';
 import HigherOrderComponents from "./children/higherOrderComponents";
 import './option03.scss';
 import Example from './children/example';
+import ParagraphView from './children/paragraphView';
+import Backbone from 'backbone';
+import Example2 from './children/example2';
+
 
 let Colums = fragments.col;
 let Glossary = fragments.gloss;
 let animals = [
     {
         id: 1,
+        cid: 1,
         term: 'horse',
         description: '跑得快'
     },
     {
         id: 2,
+        cid: 2,
         term: 'chicken',
         description: '烤来吃'
     },
@@ -32,6 +38,13 @@ class Option03 extends React.Component {
     }
 
     render() {
+        const collection = new Backbone.Collection([
+            new Backbone.Model({ text: 'A' }),
+            new Backbone.Model({ text: 'B' }),
+            new Backbone.Model({ text: 'C' })
+        ]);
+
+
         // 直接使用ref={this.fancyButtonRef}
         return (
             <div className='option03'>
@@ -49,7 +62,8 @@ class Option03 extends React.Component {
                 </div>
                 <div className='right'>
                     <Example></Example>
-
+                    <ParagraphView collection={collection}></ParagraphView>
+                    <Example2></Example2>
                 </div>
             </div>
         );
